ef720d5267e100d52310518a38970f26158e6f2f
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-09-29 11:32:15 +0300

    Fixed slug and added filter

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -11,19 +11,26 @@ class Pet(models.Model):
     personal_photo = models.URLField()
     date_of_birth = models.DateField(
         blank=True,
-        null=True
+        null=True,
     )
     slug = models.SlugField(
         unique=True,
         null=False,
-        blank=True
+        blank=True,
+        editable=False,
     )
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
         if not self.slug:
             self.slug = slugify(f"{self.name}-{self.id}")
 
-        super().save(*args, **kwargs)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 
